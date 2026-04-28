d5f246edeae424b29ef188a081d9838b0ced1998
JessDev <jeessdev@gmail.com>
2026-04-27 22:26:02 -0300

    feat: implementa funcionalidades do carrinho (adicionar, listar, total e remover produtos)

--- carrinho_de_compras/src/Main.java
+++ carrinho_de_compras/src/Main.java
@@ -21,8 +21,53 @@ public class Main {
 
             opcao = scanner.nextInt();
             scanner.nextLine();
+
+            if (opcao == 1) {
+                System.out.println("Nome do produto: ");
+                String nome = scanner.nextLine();
+
+                System.out.println("Preço: ");
+                double preco = scanner.nextDouble();
+
+                nomes[quantidadeProdutos] = nome;
+                precos[quantidadeProdutos] = preco;
+
+                quantidadeProdutos++;
+
+            } else if (opcao == 2) {
+                for (int i = 0; i < quantidadeProdutos; i++) {
+                    System.out.println(i + " - " + nomes[i] + " R$ " + precos[i]);
+                }
+
+            } else if (opcao == 3) {
+                double total = 0;
+
+                for (int i = 0; i < quantidadeProdutos; i++) {
+                    total += precos[i];
+                }
+
+                System.out.println("Total: R$ " + total);
+
+            } else if (opcao == 4) {
+                System.out.print("Digite o índice para remover: ");
+                int indice = scanner.nextInt();
+
+                if (indice >= 0 && indice < quantidadeProdutos) {
+
+                    for (int i = indice; i < quantidadeProdutos - 1; i++) {
+                        nomes[i] = nomes[i + 1];
+                        precos[i] = precos[i + 1];
+                    }
+
+                    quantidadeProdutos--;
+
+                } else {
+                    System.out.println("Índice inválido!");
+                }
+            }
+
         } while (opcao != 0);
 
-            scanner.close();
-        }
+        scanner.close();
     }
+}
